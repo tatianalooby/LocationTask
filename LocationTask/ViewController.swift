8c5a7dd54d028b9d57e33f4cdb62b62a97e7b594
tatianalooby <tatianalooby@gmail.com>
2017-03-04 12:55:49 +0300

    Initial Commit

---
+++ LocationTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LocationTask
+//
+//  Created by Tatiana Looby on 04/03/2017.
+//  Copyright © 2017 Tatiana Looby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
