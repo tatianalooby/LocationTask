eea8cf58acf4d1127e08c69b071d2a449bff3fd2
tatianalooby <tatianalooby@gmail.com>
2017-03-12 03:47:25 +0300

    Initial commit

--- LocationTask/ViewController.swift
+++ LocationTask/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var displayLocationLabel: UILabel!
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +24,39 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print ("Error: " + error.localizedDescription)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        CLGeocoder().reverseGeocodeLocation(manager.location!) { (placemarks, error) in
+            if (error != nil) {
+                print("Error: " + error!.localizedDescription)
+                return
+            }
+            if placemarks!.count > 0 {
+                let placemark = placemarks![0] as CLPlacemark
+                self.displayLocationDetails(placemark: placemark, location: manager.location!)
+            } else {
+                print("Error retrieving data")
+            }
+        }
+    }
+    
+    func displayLocationDetails(placemark: CLPlacemark, location: CLLocation) {
+        locationManager.stopUpdatingLocation()
+        displayLocationLabel.text = "Latitude: \(location.coordinate.latitude)\nLongitude: \(location.coordinate.longitude)\nLocality: \(placemark.locality!). Postal Code: \(placemark.postalCode!)\nAdministrative Area: \(placemark.administrativeArea!)\nCountry: \(placemark.country!)"
+    }
+    
+    @IBAction func showMyLocationButtonTapped(_ sender: UIButton) {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        print("button tapped")
+    }
 
 }
 
